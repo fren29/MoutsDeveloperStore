fc068391b26731e431677b4db8a190edd712f7c6
fren29 <rodrigofassa29@gmail.com>
2025-04-06 21:07:27 -0300

    fix(application): make DeleteSaleRequest implement IRequest<Unit>

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleRequest.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleRequest.cs
@@ -2,7 +2,7 @@
 
 namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale
 {
-    public class DeleteSaleRequest : IRequest
+    public class DeleteSaleRequest : IRequest<Unit>
     {
         public Guid Id { get; set; }
     }
