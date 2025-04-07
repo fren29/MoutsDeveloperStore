8774b28a56d6eda5eea7f1363caed11395623997
fren29 <rodrigofassa29@gmail.com>
2025-04-06 22:18:30 -0300

    refactor(api): update SalesController to use WebApi DTOs

--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/SalesController.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/SalesController.cs
@@ -1,6 +1,12 @@
 ﻿using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
 using Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+using Ambev.DeveloperEvaluation.Application.Sales.UpdateSale;
 using Ambev.DeveloperEvaluation.Domain.Exceptions;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.DeleteSale;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.UpdateSale;
+using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,17 +17,38 @@ namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales
     public class SalesController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
 
-        public SalesController(IMediator mediator)
+        public SalesController(IMediator mediator, IMapper mapper)
         {
             _mediator = mediator;
+            _mapper = mapper;
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody] CreateSaleCommand request)
+        public async Task<IActionResult> Create([FromBody] CreateSaleRequest request)
         {
-            var result = await _mediator.Send(request);
-            return CreatedAtAction(nameof(Get), new { id = result.Id }, result);
+            var command = _mapper.Map<CreateSaleCommand>(request);
+            var result = await _mediator.Send(command);
+            var response = _mapper.Map<CreateSaleResponse>(result);
+            return CreatedAtAction(nameof(Get), new { id = response.Id }, response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateSaleRequest request)
+        {
+            var command = _mapper.Map<UpdateSaleCommand>(request);
+            var result = await _mediator.Send(command);
+            var response = _mapper.Map<UpdateSaleResponse>(result);
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] DeleteSaleRequest request)
+        {
+            var command = _mapper.Map<DeleteSaleCommand>(request);
+            await _mediator.Send(command);
+            return NoContent();
         }
 
         [HttpGet("{id}")]
